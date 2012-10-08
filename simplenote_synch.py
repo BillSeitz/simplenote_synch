5f5f24b53bf53fea774178dc3f0c6b14b599fbab
Bill Seitz <bill@fluxent.com>
2012-10-07 19:10:45 -0500

    detab

--- simplenote_synch.py
+++ simplenote_synch.py
@@ -46,177 +46,177 @@ pfile_raw_name = 'simplenote_raw.pkl' # just raw get_note_list() results
 pfile_raw = os.path.join(path, pfile_raw_name)
 
 def pickleread():
-	print 'doing pickleread'
-	pkl_file = open(pfile_full, 'rb')
-	global last_synch_finish, notes, name_keys
-	last_synch_finish = cPickle.load(pkl_file)
-	notes = cPickle.load(pkl_file)
-	name_keys = cPickle.load(pkl_file)
-	pkl_file.close()
-	return (last_synch_finish, notes, name_keys)
-	
+    print 'doing pickleread'
+    pkl_file = open(pfile_full, 'rb')
+    global last_synch_finish, notes, name_keys
+    last_synch_finish = cPickle.load(pkl_file)
+    notes = cPickle.load(pkl_file)
+    name_keys = cPickle.load(pkl_file)
+    pkl_file.close()
+    return (last_synch_finish, notes, name_keys)
+    
 def picklewrite(last_synch_finish, notes, name_keys):
-	print 'doing picklewrite'
-	pkl_file = open(pfile_full, 'wb')
-	cPickle.dump(last_synch_finish, pkl_file)
-	cPickle.dump(notes, pkl_file)
-	cPickle.dump(name_keys, pkl_file)
-	pkl_file.close()
+    print 'doing picklewrite'
+    pkl_file = open(pfile_full, 'wb')
+    cPickle.dump(last_synch_finish, pkl_file)
+    cPickle.dump(notes, pkl_file)
+    cPickle.dump(name_keys, pkl_file)
+    pkl_file.close()
 
 def map_update(note): # update the map dictionaries - need key, content, name, and moddate
-	global last_synch_finish, notes, name_keys
-	#key = note['key']
-	#name = note['name']
-	#print 'key, name', key, name
-	#print 'notes', notes
-	notes[note['key']] = note
-	name_keys[note['name']] = note['key']
-	
+    global last_synch_finish, notes, name_keys
+    #key = note['key']
+    #name = note['name']
+    #print 'key, name', key, name
+    #print 'notes', notes
+    notes[note['key']] = note
+    name_keys[note['name']] = note['key']
+    
 def push_new(filename, moddate): # push new local file into cloud for first time
-	content = filename + '\n\n' + filename.read()
-	n = {'content': content, 'modifydate': moddate}
-	note_ret = SimpleNote.add_note(n)
-	note_ret['name'] = filename
-	map_update(note)
+    content = filename + '\n\n' + filename.read()
+    n = {'content': content, 'modifydate': moddate}
+    note_ret = SimpleNote.add_note(n)
+    note_ret['name'] = filename
+    map_update(note)
 
 def push_file(fname): # push local file to cloud - may be new or change
-	global last_synch_finish, notes, name_keys
-	f_full = os.path.join(path_text, fname)
-	f_moddate = os.path.getmtime(f_full)
-	f_content = file(f_full).read()
-	n_content = fname + '\n\n' + f_content
-	if fname in name_keys: # already in map, so already in cloud
-		print fname, 'already in cloud, updating'
-		key = name_keys[fname]
-		note = notes[key]
-		note['modifydate'] = f_moddate
-		note['content'] = n_content
-		(note, status) = simplenote.update_note(note)
-		if status > -1:
-			print fname, 'updated in cloud, updating map'
-			notes[key]['modifydate'] = f_moddate
-		else:
-			print fname, 'update to cloud failed!'
-	else: # adding to cloud for first time
-		print fname, 'not in cloud, adding for first time now'
-		note = {'content': n_content, 'modifydate': f_moddate}
-		(note, status) = simplenote.update_note(note)
-		if status > -1: 
-			print fname, 'added to cloud, now adding to map'
-			note['name'] = fname
-			map_update(note)
-			
+    global last_synch_finish, notes, name_keys
+    f_full = os.path.join(path_text, fname)
+    f_moddate = os.path.getmtime(f_full)
+    f_content = file(f_full).read()
+    n_content = fname + '\n\n' + f_content
+    if fname in name_keys: # already in map, so already in cloud
+        print fname, 'already in cloud, updating'
+        key = name_keys[fname]
+        note = notes[key]
+        note['modifydate'] = f_moddate
+        note['content'] = n_content
+        (note, status) = simplenote.update_note(note)
+        if status > -1:
+            print fname, 'updated in cloud, updating map'
+            notes[key]['modifydate'] = f_moddate
+        else:
+            print fname, 'update to cloud failed!'
+    else: # adding to cloud for first time
+        print fname, 'not in cloud, adding for first time now'
+        note = {'content': n_content, 'modifydate': f_moddate}
+        (note, status) = simplenote.update_note(note)
+        if status > -1: 
+            print fname, 'added to cloud, now adding to map'
+            note['name'] = fname
+            map_update(note)
+            
 def cloud_raw_list_grab(): # return (possibly-cached) raw list of notes - doesn't include name/content
-	if os.path.exists(pfile_raw):
-		print 'using cached pickle of raw notes'
-		pkl_file = open(pfile_raw, 'rb')
-		c_notes = cPickle.load(pkl_file)
-		pkl_file.close()
-		return c_notes
-	else:
-		print 'calling get_note_list()'
-		(c_notes, status) = simplenote.get_note_list()
-		if status == -1:
-			print 'cloud_raw_list_grab failed', status, c_notes
-			return False
-		else:
-			pkl_file = open(pfile_raw, 'wb')
-			cPickle.dump(c_notes, pkl_file)
-			pkl_file.close()
-			return c_notes
+    if os.path.exists(pfile_raw):
+        print 'using cached pickle of raw notes'
+        pkl_file = open(pfile_raw, 'rb')
+        c_notes = cPickle.load(pkl_file)
+        pkl_file.close()
+        return c_notes
+    else:
+        print 'calling get_note_list()'
+        (c_notes, status) = simplenote.get_note_list()
+        if status == -1:
+            print 'cloud_raw_list_grab failed', status, c_notes
+            return False
+        else:
+            pkl_file = open(pfile_raw, 'wb')
+            cPickle.dump(c_notes, pkl_file)
+            pkl_file.close()
+            return c_notes
 
 def map_create(): # dump any existing pickle file of maps and create new one
-	global last_synch_finish, notes, name_keys
-	if os.path.exists(pfile_full):
-		os.remove(pfile_full)
-	last_synch_finish = 0
-	notes = {}
-	name_keys = {}
-	print 'getting notes now'
-	c_notes = cloud_raw_list_grab()
-	if not c_notes:
-		print 'failed, exiting'
-	else:
-		print 'got %d notes' % (len(c_notes))
-		i = 0
-		for note in c_notes:
-			i = i+1
-			print 'Getting note %d %s' % (i, note['key'])
-			(c_note, status) = simplenote.get_note(note['key'])
-			if status == -1:
-				print 'failed', status, c_note
-				break
-			print 'Success getting note %d %s' % (i, note['key'])
-			#c_note = c_note[0]
-			name = c_note['content'].split('\n',1)[0]
-			print "Note %s has name %s" % (note['key'], name)
-			c_note['name'] = name
-			c_mod = float(c_note['modifydate'])
-			if c_mod > last_synch_finish:
-				last_synch_finish = c_mod
-				print 'Updated last_synch_finish to %d' % (last_synch_finish)
-			map_update(c_note)
-		picklewrite(last_synch_finish, notes, name_keys)
+    global last_synch_finish, notes, name_keys
+    if os.path.exists(pfile_full):
+        os.remove(pfile_full)
+    last_synch_finish = 0
+    notes = {}
+    name_keys = {}
+    print 'getting notes list now'
+    c_notes = cloud_raw_list_grab()
+    if not c_notes:
+        print 'failed, exiting'
+    else:
+        print 'got %d notes metadata, starting scraping' % (len(c_notes))
+        i = 0
+        for note in c_notes:
+            i = i+1
+            print 'Getting note %d %s' % (i, note['key'])
+            (c_note, status) = simplenote.get_note(note['key'])
+            if status == -1:
+                print 'failed', status, c_note
+                break
+            print 'Success getting note %d %s' % (i, note['key'])
+            #c_note = c_note[0]
+            name = c_note['content'].split('\n',1)[0]
+            print "Note %s has name %s" % (note['key'], name)
+            c_note['name'] = name
+            c_mod = float(c_note['modifydate'])
+            if c_mod > last_synch_finish:
+                last_synch_finish = c_mod
+                print 'Updated last_synch_finish to %d' % (last_synch_finish)
+            map_update(c_note)
+        picklewrite(last_synch_finish, notes, name_keys)
 
 def map_show(): # show what's been cached in map, assuming all consistent
-	pickleread()
-	print 'last_synch_finish', last_synch_finish
-	print len(notes), 'notes'
-	print len(name_keys), 'keys'
-	fname = 'Journal2012'
-	key = name_keys[fname]
-	print 'note', fname, notes[key]
+    pickleread()
+    print 'last_synch_finish', last_synch_finish
+    print len(notes), 'notes'
+    print len(name_keys), 'keys'
+    fname = 'Journal2012'
+    key = name_keys[fname]
+    print 'note', fname, notes[key]
 
 def moddate_compare(): 
-	"""
-	Goal: pick a file previously synched, that has a simple name, compare timestamps
-	File chosen: MusicLesson - last edited Aug05, synched since then
-	"""
-	fname = "MusicLesson"
-	pickleread()
-	key = name_keys[fname]
-	c_moddate = notes[key]['modifydate']
-	l_moddate = os.path.getmtime(os.path.join(path_text, fname))
-	import time
-	print 'current time', time.time()
-	print fname, 'cloud, local', c_moddate, l_moddate #outcome - match at 1344182336.000000
-	fname = 'Journal2012'
-	j_moddate = os.path.getmtime(os.path.join(path_text, fname))
-	print fname, j_moddate
+    """
+    Goal: pick a file previously synched, that has a simple name, compare timestamps
+    File chosen: MusicLesson - last edited Aug05, synched since then
+    """
+    fname = "MusicLesson"
+    pickleread()
+    key = name_keys[fname]
+    c_moddate = notes[key]['modifydate']
+    l_moddate = os.path.getmtime(os.path.join(path_text, fname))
+    import time
+    print 'current time', time.time()
+    print fname, 'cloud, local', c_moddate, l_moddate #outcome - match at 1344182336.000000
+    fname = 'Journal2012'
+    j_moddate = os.path.getmtime(os.path.join(path_text, fname))
+    print fname, j_moddate
 
 def push_local_to_cloud(moddate=1344182336.0): # push all files modded since a time into cloud - some will be new, some will be changes
-	global last_synch_finish, notes, name_keys
-	pickleread()
-	flist = os.listdir(path_text)
-	for fname in flist:
-		f_full = os.path.join(path_text, fname)
-		if os.path.isfile(f_full):
-			if fname == '.DS_Store':
-				continue
-			if os.path.getmtime(f_full) > moddate:
-				print 'pushing', fname
-				if os.path.getmtime(f_full) > last_synch_finish:
-					last_synch_finish = os.path.getmtime(f_full)
-				push_file(fname)
-	picklewrite(last_synch_finish, notes, name_keys)
+    global last_synch_finish, notes, name_keys
+    pickleread()
+    flist = os.listdir(path_text)
+    for fname in flist:
+        f_full = os.path.join(path_text, fname)
+        if os.path.isfile(f_full):
+            if fname == '.DS_Store':
+                continue
+            if os.path.getmtime(f_full) > moddate:
+                print 'pushing', fname
+                if os.path.getmtime(f_full) > last_synch_finish:
+                    last_synch_finish = os.path.getmtime(f_full)
+                push_file(fname)
+    picklewrite(last_synch_finish, notes, name_keys)
 
 def last_synch_read(): # this is start of some future code
-	(last_synch_finish, notes, name_keys) = pickleread()
-	# local_changed = list of files mod since last_synch_finish # filename and moddate
-	# cloud_changed = list of notes mod since last_synch_finish # initially just key and moddate
-	for f in local_changed:
-		if f not in name_keys.keys():
-			push_new(f)
+    (last_synch_finish, notes, name_keys) = pickleread()
+    # local_changed = list of files mod since last_synch_finish # filename and moddate
+    # cloud_changed = list of notes mod since last_synch_finish # initially just key and moddate
+    for f in local_changed:
+        if f not in name_keys.keys():
+            push_new(f)
 
 from simplenote import Simplenote # http://pypi.python.org/pypi/simplenote/0.2.0
 simplenote = Simplenote(username, password)
 
 def cloud_list_create():
-	(notes, status) = simplenote.get_note_list() # have to get the whole list
-	
+    (notes, status) = simplenote.get_note_list() # have to get the whole list
+    
 if __name__ == '__main__':
-	# map_create()
-	# moddate_compare()
-	map_show()
-	# push_local_to_cloud()
-	
+    # map_create()
+    # moddate_compare()
+    map_show()
+    # push_local_to_cloud()
+    
